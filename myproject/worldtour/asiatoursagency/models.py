3987d5d14d8c0aa33a2c69a4ff154bd9f22a81a9
suvarna1405 <suvarna7404@gmail.com>
2025-06-28 00:25:03 +0530

    django is cool

--- myproject/worldtour/asiatoursagency/models.py
+++ myproject/worldtour/asiatoursagency/models.py
@@ -6,3 +6,7 @@ class Tour(models.Model):
     destination_country = models.CharField(max_length=64)
     number_of_nights =models.IntegerField()
     price = models.IntegerField()
+
+    #this is  a string representation of the tour
+    def __str__(self):
+        return (f"ID:{self.id}: From {self.origin_country} To {self.destination_country}, {self.number_of_nights} nights costs ${self.price}")
--- myproject/worldtour/asiatoursagency/views.py
+++ myproject/worldtour/asiatoursagency/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Tour
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Asia Tours Agency")
+    tours=Tour.objects.all()
+    context = {'tours':tours}
+    return render(request,'tours/index.html',context)
